2fe91029b90ea6df83427ec7ae26d749699b5b55
Tony Hoang <tonyh@hydrabyte.co>
2025-11-20 12:49:53 +0700

    change config

--- services/aiwm/src/app/app.module.ts
+++ services/aiwm/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
-import { HealthModule, JwtStrategy, CorrelationIdMiddleware } from '@hydrabyte/base';
+import {
+  HealthModule,
+  JwtStrategy,
+  CorrelationIdMiddleware,
+} from '@hydrabyte/base';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NodeModule } from '../modules/node/node.module';
@@ -18,7 +22,6 @@ import { ProcessorsModule } from '../queues/processors.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'services/aiwm/.env',
     }),
     MongooseModule.forRoot(
       process.env['MONGODB_URI'] || 'mongodb://localhost:27017/hydra-aiwm'
